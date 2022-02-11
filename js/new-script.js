c956450ad504bc55aef46e07344d5c854f405cb1
mni-dev <nazrulislam01531@gmail.com>
2022-02-11 11:51:48 +0600

    Submit button

--- js/new-script.js
+++ js/new-script.js
@@ -1,2 +1,47 @@
+//changing color of <h2> element
 const lightBlue = document.getElementsByTagName('h2');
-lightBlue.style.color = 'red';
+for (const heading of lightBlue) {
+    heading.style.color = 'lightCoral';
+}
+//change backgroundColor of a section
+const bgColorChange = document.getElementById('bag-section');
+bgColorChange.style.backgroundColor = 'lightgreen';
+
+//change border-radius to 30 px
+const bRadiuChange = document.getElementsByClassName('card');
+for (const card of bRadiuChange) {
+    card.style.borderRadius = '30px'
+}
+
+//function as a click-handler for a BUTTON
+/* function clickHandler (showInConsole){
+    console.log('I Love Coding');
+}
+ */
+//Remove BUTTON on click
+function disappear() {
+    const remove = document.querySelectorAll('button.special-btn');
+    remove.style.display = 'none';
+}
+
+
+//Task 7: Disable and Activate Button
+document.getElementById('email-id').addEventListener('change', function (event) {
+    const submitBtn = document.getElementById('btn-disabled');
+    if (event.target.value == 'email') {
+        console.log(event.target.value)
+        submitBtn.removeAttribute('disabled', true);
+        // submitBtn.setAttribute("class", "demoClass");
+        console.log('i love')
+    }
+    else {
+        submitBtn.setAttribute('disabled', true);
+        console.log('oops')
+    }
+})
+
+//Task 9: Double-click to change bgColor
+function doubleClick() {
+    const dblClick = document.getElementById('double-click');
+    dblClick.style.backgroundColor = 'lightBlue';
+}
